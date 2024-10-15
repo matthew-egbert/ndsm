281cc8dcdc96bbf3b70411aca7db84e5d78ae457
Matthew Egbert <matthew.egbert@gmail.com>
2024-10-15 16:13:39 +0900

    first go

--- slurm_script.bash
+++ slurm_script.bash
@@ -5,12 +5,13 @@
 #SBATCH -J JobArray                                                                           
 #SBATCH --time=00:00:15     # Walltime                                                        
 #SBATCH --mem-per-cpu=1G                                                                      
-#SBATCH --array=1      # Array definition                                                     
+#SBATCH --array=1-32      # Array definition                                                     
 #SBATCH --qos=debug          # debug QOS for high priority job tests                          
 #SBATCH --output=/home/megb269/outputs/output_%a.out                                          
 #SBATCH --error=/home/meg269/outputs/output_%a.err                                            
 
 module load Python/3.11.3-gimkl-2022a
-pwd
-echo $SLURM_ARRAY_TASK_ID
+source /home/megb269/ndsmvenv/bin/activate
+cd /home/megb269/repos/ndsm
+python3 --headless --experiment light --seed $SLURM_ARRAY_TASK_ID
 #python3 evolve.py $SLURM_ARRAY_TASK_ID        
