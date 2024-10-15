6b4dc5bfc9a928fa80f0fdcf8083d5bb2f49951a
Matthew Egbert <matthew.egbert@gmail.com>
2024-10-15 16:01:23 +0900

    s

--- slurm_script.bash
+++ slurm_script.bash
@@ -1,14 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash -e                                                                                
 
-#SBATCH --job-name LAG_evolution
-#SBATCH -A uoa04260         # Project Account
-#SBATCH -J JobArray
-#SBATCH --time=00:00:15     # Walltime
-#SBATCH --mem-per-cpu=1G
-#SBATCH --array=1-32      # Array definition
-#SBATCH --qos=debug          # debug QOS for high priority job tests
+#SBATCH --job-name LAG_evolution                                                              
+#SBATCH -A uoa04260         # Project Account                                                 
+#SBATCH -J JobArray                                                                           
+#SBATCH --time=00:00:15     # Walltime                                                        
+#SBATCH --mem-per-cpu=1G                                                                      
+#SBATCH --array=1      # Array definition                                                     
+#SBATCH --qos=debug          # debug QOS for high priority job tests                          
+#SBATCH --output=/home/megb269/outputs/output_%a.out                                          
+#SBATCH --error=/home/meg269/outputs/output_%a.err                                            
 
 module load Python/3.11.3-gimkl-2022a
 pwd
-#python3 main.py --headless --experiment light --seed $SLURM_ARRAY_TASK_ID
-#python evolve.py $SLURM_ARRAY_TASK_ID
+echo $SLURM_ARRAY_TASK_ID
+#python3 evolve.py $SLURM_ARRAY_TASK_ID        
